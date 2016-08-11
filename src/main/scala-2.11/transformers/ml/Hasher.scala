d5b39aa6fcdd8214abb52aea010537812a565bef
Janardhan <janardhanp22@gmail.com>
2016-08-11 09:32:11 -0700

    Changed for loop to functional map

--- src/main/scala-2.11/transformers/ml/Hasher.scala
+++ src/main/scala-2.11/transformers/ml/Hasher.scala
@@ -5,14 +5,13 @@ import org.apache.spark.ml.param.{IntParam, ParamValidators}
 import org.apache.spark.ml.util.Identifiable
 import org.apache.spark.sql.types.{ArrayType, DataType, LongType}
 
-import scala.collection.mutable.ArrayBuffer
 import scala.util.hashing.MurmurHash3.stringHash
 
 /**
  * Created by jshetty on 8/9/16.
  */
 class Hasher(override val uid: String) extends UnaryTransformer[Seq[String], Seq[Long], Hasher] {
-  def this() = this(Identifiable.randomUID("hasher"))
+  def this() = this(Identifiable.randomUID("Hasher"))
 
   val seed: IntParam = new IntParam(this, "seed", "seed value", ParamValidators.gtEq(1))
   def setSeed(value: Int): this.type = set(seed, value)
@@ -20,13 +19,8 @@ class Hasher(override val uid: String) extends UnaryTransformer[Seq[String], Seq
   setDefault(seed -> 32)
 
   override protected def createTransformFunc: (Seq[String]) => Seq[Long] = { shingleArray =>
-  val resultArray = new ArrayBuffer[Long]()
-  for (i <- 0 to shingleArray.length - 1) {
-    // Make it lowercase for hashing
-    resultArray += math.abs(stringHash(shingleArray(i).toLowerCase().mkString(""), seed = getSeed) % Int.MaxValue)
-  }
-  resultArray.toSeq
-}
+   shingleArray.map((str: String) => math.abs(stringHash(str.toLowerCase().mkString(""), seed=getSeed)).toLong)
+ }
 
   override protected def outputDataType: DataType = new ArrayType(LongType, false)
 
